92f494627109f1bc10f66c5612e307d6ffd211bb
Junior Samaroo <jns0987654321@gmail.com>
2016-02-04 23:07:10 -0500

    Numbers and Variables playgrounds added

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Integers
+var negNumSigned: Int = -500 //signed int
+
+var numUnsigned: UInt = 500 //unsigned non-negative numbers
+
+var reallyBigNumber: Int64 = 4565765754635454 //more than 2 billion
+
+
+//Floating Point
+var doubleDefault = 34.5 //defaults to double 64bit more precision than float 32bit
+
+var floatNum:  Float = 3.4 //Float
+
+var sum = doubleDefault * Double(floatNum) //TYPE SAFETY have to concert to double
---
+++ variables.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+///////////////////////////////////////////////////////////
+
+// Variables
+var str = "Hello, playground!!"
+var number = 20 // Integer
+var decimal = 20.5 // Decimal
+decimal = 20.8
+var one,two,three = 0
+
+// Constants
+let newDeciaml = 40.45
+newDecimal = 5 // cannot change, not mutable
+
+var newString: String // type annotation, assinging type
+newString = 5
+newString = "Hello Money! Bye!"
+
+///////////////////////////////////////////////////////////
+
+
